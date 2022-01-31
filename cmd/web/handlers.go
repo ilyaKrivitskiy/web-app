3f78b8201e2ebfbeef4f265b3895591141ef68e1
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-02-01 02:13:19 +0300

    new go file handlers; all functions from main were transported to go file handlers

---
+++ cmd/web/handlers.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/" {
+		http.NotFound(w, req)
+		return
+	}
+
+	w.Write([]byte("testing..."))
+}
+
+func showSnippet(w http.ResponseWriter, req *http.Request) {
+
+	id, err := strconv.Atoi(req.URL.Query().Get("id"))
+	if err != nil || id < 0 {
+		http.NotFound(w, req)
+		return
+	}
+
+	fmt.Fprintf(w, "This is snippet with ID %d", id)
+	//w.Write([]byte("Show snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method != http.MethodPost {
+
+		http.Error(w, "Get method not allowed!", http.StatusMethodNotAllowed)
+		//w.Header().Set("Allow", http.MethodPost)
+		//w.WriteHeader(405)
+		//w.Write([]byte("GET method is forbidden!"))
+		return
+	}
+
+	w.Write([]byte("Create snippet"))
+}
