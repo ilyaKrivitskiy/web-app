a2df6e54b97574e861acafbf0d23442bd66d85eb
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-01-31 03:23:15 +0300

    add router and test simple server

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+)
 
+func home(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("testing..."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	log.Println("Start server on http://127.0.0.1:4000")
+	errorOfServer := http.ListenAndServe(":4000", mux)
+	log.Println(errorOfServer)
 }
