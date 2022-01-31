ded93fb851221bf34b8d87593ce1efd5a64999f2
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-01-31 18:36:43 +0300

    added more routes

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, req *http.Request) {
 	w.Write([]byte("testing..."))
 }
 
+func showSnippet(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Show snippet"))
+}
+
+func createSnippet(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("Create snippet"))
+}
+
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Start server on http://127.0.0.1:4000")
 	errorOfServer := http.ListenAndServe(":4000", mux)
