0cac20c5f014cc8c5152650f003d4bec65a790c3
ilyaKrivitskiy <krivitskiy-ilya@mail.ru>
2022-02-03 15:39:44 +0300

    added new handle function to show static pages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,10 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Start server on http://127.0.0.1:4000")
 	errorOfServer := http.ListenAndServe(":4000", mux)
 	log.Fatal(errorOfServer)
